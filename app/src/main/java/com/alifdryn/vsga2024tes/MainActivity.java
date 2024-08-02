5dea2d51a96450a7d6391b027d80d71d114c3d99
alifdryn <alif.daryan@gmail.com>
2024-08-02 10:05:31 +0700

    percobaan

--- app/src/main/java/com/alifdryn/vsga2024tes/MainActivity.java
+++ app/src/main/java/com/alifdryn/vsga2024tes/MainActivity.java
@@ -1,6 +1,9 @@
 package com.alifdryn.vsga2024tes;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String FILENAME = "catatan.txt";
+
+    private EditText editText;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +34,62 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editText = findViewById(R.id.editText);
+
+        bacaFile();
+    }
+
+    private void buatFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        FileOutputStream fos;
+        try {
+            file.createNewFile();
+            fos = new FileOutputStream(file, false);
+            fos.write(editText.getText().toString().getBytes());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void bacaFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            editText.setText(text);
+        }
+        else {
+            editText.setText("");
+        }
+    }
+
+    private void hapusFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            file.delete();
+            editText.setText("");
+        }
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.button1)
+            buatFile();
+        else if (view.getId() == R.id.button2)
+            bacaFile();
+        else if (view.getId() == R.id.button4)
+            hapusFile();
     }
 }
